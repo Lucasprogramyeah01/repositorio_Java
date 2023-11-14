5ef9336d579da1e57ab5187d6a7e0b3a6cedf57d
LucasFallaUrtiaga <falla.urluc24@triana.salesianos.edu>
2023-11-14 14:22:09 +0100

    Cambios realizados

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo{
     public static void main(String[] args){
 
         System.out.println("*".repeat(10));
-        System.out.println("Hola Mundo");
+        System.out.println('JELOU JAJA Hola Mundo");
         System.out.println("*".repeat(10));
 
 
